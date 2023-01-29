43bca052a36c85a58449272441f83fabe70d5d61
Serhii Chernetskiy <sergchernetskiy@gmail.com>
2023-01-29 13:15:32 +0200

    js-modal

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,37 @@
 (() => {
-   const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
-      closeModalBtn: document.querySelector("[data-modal-close]"),
-      modal: document.querySelector("[data-modal]"),
-   };
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
-   refs.openModalBtn.addEventListener("click", toggleModal);
-   refs.closeModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtn.addEventListener("click", openModal);
+  refs.closeModalBtn.addEventListener("click", closeModal);
+  refs.modal.addEventListener("click", onBackdropClick);
 
-   function toggleModal() {
-      refs.modal.classList.toggle("is-hidden");
-   }
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+
+  function openModal() {
+    document.addEventListener("keydown", onEscKeyPress);
+    toggleModal();
+  }
+
+  function closeModal() {
+    document.removeEventListener("keydown", onEscKeyPress);
+    toggleModal();
+  }
+
+  function onBackdropClick(e) {
+    if (e.target === e.currentTarget) {
+      toggleModal();
+    }
+  }
+
+  function onEscKeyPress(e) {
+    if (e.code === "Escape") {
+      toggleModal();
+    }
+  }
 })();
